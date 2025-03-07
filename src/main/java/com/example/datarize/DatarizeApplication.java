2fa29d79eb4f3976dd9f2725ec898c85835de941
iamseung <seesungseok@naver.com>
2025-03-07 15:35:12 +0900

    first commit

---
+++ src/main/java/com/example/datarize/DatarizeApplication.java
@@ -0,0 +1,13 @@
+package com.example.datarize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatarizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatarizeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datarize/DatarizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datarize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatarizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
